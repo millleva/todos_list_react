8efb1e8c1ea0b34ffb44bc2e0e535fe583421d21
millleva <kochevamilenazp@gmail.com>
2023-01-02 22:50:45 +0200

    configure eslint + prettier

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Todo-list
 
 ## Functionality
+
 - create todo punct
 - remove todo punct
 - sort todo punct
 - paginate todos
 
 ## Tech Stack
+
 - React.js
 - Eslint, prettier
--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      it works!
-    </div>
-  );
+  return <div className="App">it works!</div>;
 }
 
 export default App;
