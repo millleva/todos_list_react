b638f08f6bdd169a1834b5862a15817fc9ce3969
millleva <kochevamilenazp@gmail.com>
2023-01-02 23:58:54 +0200

    Add basic styles for todo item

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,14 @@ import React, { useState } from "react";
 // components
 import TodoItem from "./TodoItem";
 
+// styles
+import "../styles/App.css";
+
 const App = () => {
   const [todos, setTodos] = useState([
-    { id: 1, title: "Clean hands", desc: "go to vanna", completed: false },
-    { id: 2, title: "Wash dishes", desc: "go to kitchen", completed: false },
-    { id: 3, title: "Do homework", desc: "go to lounge", completed: false },
+    { id: 1, title: "Clean hands", desc: "go to vanna", state: false },
+    { id: 2, title: "Wash dishes", desc: "go to kitchen", state: false },
+    { id: 3, title: "Do homework", desc: "go to lounge", state: false },
   ]);
 
   return (
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import "../styles/TodoItem.css";
+
 const TodoItem = () => {
-  return <div>todoitem</div>;
+  return (
+    <div className="TodoItem">
+      <h3 className="TodoItem__title">Title of TO-DO item</h3>
+      <p className="TodoItem__desc">Description of item</p>
+      <p className="TodoItem__state">State: </p>
+    </div>
+  );
 };
 
 export default TodoItem;
